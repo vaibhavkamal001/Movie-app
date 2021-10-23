3ca8a8557551bf69d2c14882dd0e3912a5c5e467
soule01 <vaibhavkamal01@gmail.com>
2021-10-23 18:02:17 +0530

    deploy

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{error:err});
 })
 
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("serving port 3000");
 })
